329bf7e88ce778f033eea930fc18c9019871acf1
javier <jav.valenzuelae@duocuc.cl>
2024-12-04 19:15:04 -0300

    css final

--- foro/views.py
+++ foro/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 from user_agents import parse
 
 def index(request):
-    # Obtener el user agent desde las cabeceras HTTP
+    # Inicializamos la variable de tipo de dispositivo
+    device_type = 'Desconocido'
+    
+    # Obtener el 'user_agent' desde las cabeceras HTTP
     user_agent = request.META.get('HTTP_USER_AGENT', '')
-    is_mobile = False
 
-    # Detectar si es un dispositivo móvil
+    # Si el 'user_agent' está presente, analizamos el tipo de dispositivo
     if user_agent:
         user_agent_parsed = parse(user_agent)
+        
+        # Determinamos el tipo de dispositivo
         if user_agent_parsed.is_mobile:
-            is_mobile = True
-
-    return render(request, 'foro/index.html', {'is_mobile': is_mobile})
+            device_type = 'Móvil'
+        elif user_agent_parsed.is_tablet:
+            device_type = 'Tablet'
+        else:
+            device_type = 'Escritorio'
+    
+    # Pasamos el tipo de dispositivo al template
+    return render(request, 'foro/index.html', {'device_type': device_type})
